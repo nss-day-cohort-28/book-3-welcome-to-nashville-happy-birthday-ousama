a7288eece312240e280c7c90edba8239039c3b51
John Wood <johnnathanwood@gmail.com>
2018-10-26 11:36:48 -0500

    merge master

--- scripts/concert.js
+++ scripts/concert.js
@@ -1,8 +1,3 @@
-// fetch("https://api.songkick.com/api/3.0/metro_areas/11104/calendar.json?apikey=qxYE8Bnx5j7Stvgg")
-//     .then((response) => response.json())
-//     .then((parsedData) => {
-//         console.table(parsedData.resultsPage.results.event)
-//     })
 
 const concerts_url = "https://api.songkick.com/api/3.0/metro_areas/11104/calendar.json?apikey=qxYE8Bnx5j7Stvgg"
 const local_api1 = "http://localhost:8088/saveItinerary"
@@ -13,30 +8,44 @@ const concerts_api = {
     }
 }
 
+// const createConcertSelectBox = (concerts) => {
+//     const concertSelectBox = document.createElement('select');
+//     concertSelectBox.setAttribute('id', 'concert_select')
+//     for (let i = 0; i < concerts.length; i++) {
+//         const concertSelectOption = document.createElement('option');
+//         concertSelectOption.setAttribute('value', concerts[i].resultsPage.results.event)
+//         concertSelectOption.textContent = concerts[i].Artist;
+//         concertSelectBox.add(concertSelectOption)
+//     }
+//     return concertSelectBox;
+// }
+
 const createConcertSelectBox = (concerts) => {
-    const concertSelectBox = document.createElement('select');
-    concertSelectBox.setAttribute('id', 'concert_select')
+    const concertSelectBox = document.createElement("select")
+    concertSelectBox.setAttribute("id", "concert_selection")
     for (let i = 0; i < concerts.length; i++) {
-        const concertSelectOption = document.createElement('option');
-        concertSelectOption.setAttribute('value', concerts[i].event)
-        concertSelectOption.textContent = concerts[i].event;
-        concertSelectBox.add(concertSelectOption)
+        const concertSelectOption = document.createElement("option")
+        concertSelectOption.setAttribute("value", concerts[0].resultsPage)
+        concertSelectOption.textContent = concerts
+        concertSelectBox.appendChild(concertSelectOption)
     }
-    return concertSelectBox;
-}
+    return concertSelectBox
+  }
+  
+  
 
 concerts_api.getConcertSelection()
     .then((concerts) => {
         console.table(concerts)
 
         let concertSelectionFunction = createConcertSelectBox(concerts)
-        console.log(concertSelectionFunction)
+        // console.log(concertSelectionFunction)
         document.getElementById("concerts_select").appendChild(concertSelectionFunction)
     })
 
 document.querySelector(".itinerary__save").addEventListener("click", event => {
 
-    const concertInput = document.querySelector("#concerts_select").value
+    const concertInput = document.querySelector("#concerts_selection").value
 
     const saveItinerary = {
         concert_name: concertInput
